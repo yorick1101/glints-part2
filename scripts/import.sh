8978c12ceb829b1c31fcd9f8f25402d3810939ee
yorick1101 <yorick1101@github.com>
2019-12-11 11:43:09 +0800

    bug fix

--- scripts/import.sh
+++ scripts/import.sh
@@ -6,4 +6,12 @@ db_host=mongo
 db_port=27017
 db_name=glints
 
+if [ "$#" -ne 1 ];then 
+   echo "import.sh [path to json file to import]; args number:$#"
+   exit -1
+elif [ ! -f "$1" ];then 
+   echo "$1 is not a file or not exists"
+   exit -1
+fi
+
 docker run --network=$network -e ENV_DB_USER=$db_user -e ENV_DB_PASSWORD=$db_password -e ENV_DB_HOST=$db_host -e ENV_DB_PORT=$db_port -e ENV_DB_NAME=$db_name -v $1:/app/companies.json glints-part2-etl:latest /app/etl.exe /app/companies.json
